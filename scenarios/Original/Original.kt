6f8d4f3ab87c9cc973f50297c47642ce6d2e810f
ncardozo <n.cardozo@uniandes.edu.co>
2022-01-21 10:29:16 -0500

    adding original versions for the mobile languages
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

---
+++ scenarios/Original/Original.kt
@@ -0,0 +1,12 @@
+class Original {
+
+	fun sumProd(n: Int) {
+		var sum = 0.0;//C1
+		var prod = 1.0;
+		for(var i = 1; i <= n; i++) {
+			sum = sum + i;
+			prod = prod * i;
+			foo(sum, prod);
+		}
+	}
+}
